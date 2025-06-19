7e00d2dad742224eb6bd7e613d9e84ba3c09fe48
Rishikesh G <104022405+RishiSpace@users.noreply.github.com>
2025-06-19 14:39:56 +0000

    Routes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -20,7 +20,7 @@ const About: React.FC = () => {
           </p>
         </div>
         <div className="about-image">
-          <img src="/media/rishi-pro.png" alt="Profile" className="profile-image" />
+          <img src="/landing-vite/media/rishi-pro.png" alt="Profile" className="profile-image" />
         </div>
       </div>
     </section>
