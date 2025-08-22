1165d7d010f2d368a72b1434da4520728d20a3a7
Rishikesh G <104022405+RishiSpace@users.noreply.github.com>
2025-08-22 20:14:55 +0530

    perhaps this should work ?

--- fire.js
+++ fire.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebas
   import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
 
   const firebaseConfig = {
-    apiKey: import.meta.env.VITE_ENV_API_KEY,
-    authDomain: import.meta.env.VITE_ENV_AUTH_DOMAIN,
-    databaseURL: import.meta.env.VITE_ENV_DATABASE_URL,
-    projectId: import.meta.env.VITE_ENV_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_ENV_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_ENV_MESSAGING_SENDERID,
-    appId: import.meta.env.VITE_ENV_APPID
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
+    appId: import.meta.env.VITE_APPID
   };
 
   const app = initializeApp(firebaseConfig);
